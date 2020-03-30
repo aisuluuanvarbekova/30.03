78c8525f07a465d4152c966479363dd33b6ccc2a
aisuluuanvarbekova <aischika1992@gmail.com>
2020-03-31 00:10:15 +0600

    hw2

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printAll();
+        printAll();
+        printAll();
+        printAll();
+        printAll();
+
+
+    }
+    public static void printAll(){
+        System.out.println("Java");
+        System.out.println("Android");
+
+
+    }
+}
